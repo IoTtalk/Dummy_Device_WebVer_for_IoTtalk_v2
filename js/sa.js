2f3c0cbfd46d66b7896efef07200a3fb663d2087
Guan Yu , Chen <cjs358a@gmail.com>
2021-06-13 18:48:33 +0800

    new html and js

--- js/sa.js
+++ js/sa.js
@@ -1,26 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const idf = document.querySelector('#idf');
-  const odf = document.querySelector('#odf');
+  function createRow(id, number) {
+    const row = document.createElement('tr');
+    const block1 = document.createElement('td');
+    const block2 = document.createElement('td');
+    const value = document.createTextNode(number);
+    const timeStr = new Date().toLocaleTimeString(
+      'zh-TW',
+      {
+        hour12: false,
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        fractionalSecondDigits: 3,
+      },
+    );
+
+    const time = document.createTextNode(timeStr);
+
+    block1.appendChild(time);
+    block2.appendChild(value);
+    row.appendChild(block1);
+    row.appendChild(block2);
+
+    const table = document.getElementById(id);
+    table.appendChild(row);
+
+    if (table.rows.length > 10 + 1) {
+      table.deleteRow(1);
+    }
+  }
 
   function Dummy_Sensor() {
     const number = Math.floor((1 + Math.random()) * 0x10000);
-    idf.value = number;
+    createRow('dummy-sensor', number);
     return [number];
   }
 
   function Dummy_Control(data) {
-    [odf.value] = data;
+    createRow('dummy-control', data);
   }
 
   const option = {
-    apiUrl: 'http://IP:9992',
+    apiUrl: 'https://iottalk2.tw/csm',
     deviceModel: 'Dummy_Device',
     deviceName: 'MyDummyDevice',
     idfList: [[Dummy_Sensor, ['int']]],
-    odfList: [[Dummy_Control, ['int']]],
+    odfList: [Dummy_Control],
     pushInterval: 0,
     interval: {
-      Dummy_Sensor: 1,
+      Dummy_Sensor: 1 / 24,
     },
   };
 
